5a3dfd2a55b9e7ac304e9309ce1a0844f3e33239
Arman Haçikoğlu <armanhacikoglu@gmail.com>
2021-06-23 13:57:21 +0300

    FalconEnemyStateComponent

--- Plugins/Phasmid/Source/Phasmid/Private/FalconEnemyStateComponent.cpp
+++ Plugins/Phasmid/Source/Phasmid/Private/FalconEnemyStateComponent.cpp
@@ -14,6 +14,49 @@ UFalconEnemyStateComponent::UFalconEnemyStateComponent()
 }
 
 
+bool UFalconEnemyStateComponent::AllLootCollected()
+{
+	return false;
+}
+
+void UFalconEnemyStateComponent::BP_AddActorToWatchList(AActor * Actor, FName NextStateName)
+{
+}
+
+void UFalconEnemyStateComponent::BP_AddActorToWatchListByIndex(AActor * Actor, uint8 Index)
+{
+}
+
+void UFalconEnemyStateComponent::BP_OnEnterState()
+{
+}
+
+void UFalconEnemyStateComponent::BP_OnExitState(UObject * NextState)
+{
+}
+
+void UFalconEnemyStateComponent::BP_SetTarget(UObject * Actor)
+{
+}
+
+void UFalconEnemyStateComponent::BP_SetWaypoints(UObject * Points)
+{
+}
+
+void UFalconEnemyStateComponent::BP_UpdateWanderParams(float TurnRate, float OuterRadius, float Delay)
+{
+}
+
+FName UFalconEnemyStateComponent::GetStateName()
+{
+	return FName();
+}
+
+bool UFalconEnemyStateComponent::WillSpawnCollectibleOfType(UClass *& Class)
+{
+	return false;
+}
+
 // Called when the game starts
 void UFalconEnemyStateComponent::BeginPlay()
 {
--- Plugins/Phasmid/Source/Phasmid/Public/FalconEnemyStateComponent.h
+++ Plugins/Phasmid/Source/Phasmid/Public/FalconEnemyStateComponent.h
@@ -16,6 +16,48 @@ public:
 	// Sets default values for this component's properties
 	UFalconEnemyStateComponent();
 
+	UFUNCTION(BlueprintCallable)
+	static bool AllLootCollected();	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_AddActorToWatchList(AActor* Actor, FName NextStateName);	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_AddActorToWatchListByIndex(AActor* Actor, uint8 Index);	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_OnEnterState();	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_OnExitState(UObject* NextState);	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_SetTarget(UObject* Actor);	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_SetWaypoints(UObject* Points);	
+	
+	UFUNCTION(BlueprintCallable)
+	static void BP_UpdateWanderParams(float TurnRate, float OuterRadius, float Delay);
+
+	UFUNCTION(BlueprintCallable)
+	static FName GetStateName();	
+	
+	UFUNCTION(BlueprintCallable)
+	static bool WillSpawnCollectibleOfType(UClass*& Class);
+	
+
+	/*
+	MulticastDelegateProperty Phasmid.FalconEnemyStateComponent.OnProjectileSpawned
+	MulticastDelegateProperty Phasmid.FalconEnemyStateComponent.OnExitStateDelegate
+	MulticastDelegateProperty Phasmid.FalconEnemyStateComponent.OnEnterStateDelegate
+	MulticastDelegateProperty Phasmid.FalconEnemyStateComponent.OnCollectibleSpawned
+	MulticastDelegateProperty Phasmid.FalconEnemyStateComponent.OnActorSpawned
+	
+	
+	StructProperty Phasmid.FalconEnemyStateComponent.State Probably related to FalconEverythingTrigger??? maybe
+	*/
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
